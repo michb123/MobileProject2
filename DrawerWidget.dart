20b6c3a416a7580209ecaf13cfe55a0242f566da
michb123 <132993161+michb123@users.noreply.github.com>
2023-11-28 00:49:09 +0200

    Final DrawerWidget.dart

--- DrawerWidget.dart
+++ DrawerWidget.dart
@@ -1,96 +1,85 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'HomePage.dart';
+import 'AccountPage.dart';
+import 'Orderlist.dart';
 class DrawerWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context){
- return Drawer(
-   child: ListView(
-     children: [
+    return Drawer(
+      child: ListView(
+        children: [
           DrawerHeader(
-           padding: EdgeInsets.zero,
+            padding: EdgeInsets.zero,
             child: UserAccountsDrawerHeader(
               decoration:BoxDecoration(
                 color: Colors.red,
               ) ,
-                accountName: Text(
-                  "Programmer",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+              accountName: Text(
+                "Programmer",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
+              ),
+              accountEmail: Text(
+                "dearprogrammer@gmail.com",
+                style: TextStyle(
+                  fontSize: 16,
                 ),
-               accountEmail: Text(
-                   "dearprogrammer@gmail.com",
-                 style: TextStyle(
-                   fontSize: 16,
-                 ),
-               ),
+              ),
               currentAccountPicture: CircleAvatar(backgroundImage: AssetImage("images/avatar.jpg"),),
             ),
           ),
-         // List Title
-         ListTile(
-           leading: Icon(
-             CupertinoIcons.home,
-             color: Colors.red,
-           ),
-           title: Text("Home", style: TextStyle(
-             fontSize: 18, fontWeight: FontWeight.bold,
-           ),),
-         ),
+          // List Title
+          ListTile(
+            leading: Icon(
+              CupertinoIcons.home,
+              color: Colors.red,
+            ),
+            title: Text("Home", style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold,
+            ),),onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          },
+          ),
 
-       ListTile(
-         leading: Icon(
-           CupertinoIcons.person,
-           color: Colors.red,
-         ),
-         title: Text("My Account", style: TextStyle(
-           fontSize: 18, fontWeight: FontWeight.bold,
-         ),),
-       ),
+          ListTile(
+            leading: Icon(
+              CupertinoIcons.person,
+              color: Colors.red,
+            ),
+            title: Text("My Account", style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold,
+            ),),onTap: () {
+            // Navigate to the Home page
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => RegisterPage()),
+            );
+          },
+          ),
 
-       ListTile(
-         leading: Icon(
-           CupertinoIcons.cart_fill,
-           color: Colors.red,
-         ),
-         title: Text("My orders", style: TextStyle(
-           fontSize: 18, fontWeight: FontWeight.bold,
-         ),),
-       ),
-
-       ListTile(
-         leading: Icon(
-           CupertinoIcons.heart_fill,
-           color: Colors.red,
-         ),
-         title: Text("My Wish List", style: TextStyle(
-           fontSize: 18, fontWeight: FontWeight.bold,
-         ),),
-       ),
-
-       ListTile(
-         leading: Icon(
-           CupertinoIcons.settings,
-           color: Colors.red,
-         ),
-         title: Text("Settings", style: TextStyle(
-           fontSize: 18, fontWeight: FontWeight.bold,
-         ),),
-       ),
-
-       ListTile(
-         leading: Icon(
-           Icons.exit_to_app,
-           color: Colors.red,
-         ),
-         title: Text("Log Out", style: TextStyle(
-           fontSize: 18, fontWeight: FontWeight.bold,
-         ),),
-       ),
-
-   ],),
- );
+          ListTile(
+            leading: Icon(
+              CupertinoIcons.cart_fill,
+              color: Colors.red,
+            ),
+            title: Text("My orders", style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold,
+            ),),onTap: () {
+            // Navigate to the Home page
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => OrderPage()),
+            );
+          },
+          ),
+        ],),
+    );
   }
 
 }
