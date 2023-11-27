b69570a8f0e6db6d2362e2f623f89ab888f677d0
michb123 <132993161+michb123@users.noreply.github.com>
2023-11-28 00:54:26 +0200

    Final NewestItemWidget.dart

--- NewestItemWidget.dart
+++ NewestItemWidget.dart
@@ -11,94 +11,94 @@ class   NewestItemWidget extends StatelessWidget{
             //for(int i=0 ; i<1; i++)
             //Single items
             for(int i=0 ; i<1; i++)
-          Padding(padding: EdgeInsets.symmetric(vertical: 10 ),
-            child: Container(
-              width: 380 ,
-              height: 150 ,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 3,
-                    blurRadius: 10,
-                    offset: Offset(0,3),
-                  )
-                ]
-              ),
-              child: Row(
-                children: [
-                  InkWell(
-                  onTap: (){},
-                    child: Container(
-                      alignment: Alignment.center,
-                      child: Image.asset(
-                        "images/burger.png",
-                        height: 120,
-                        width: 150,
-                      ),
-                    ),
+              Padding(padding: EdgeInsets.symmetric(vertical: 10 ),
+                child: Container(
+                  width: 380 ,
+                  height: 150 ,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 3,
+                          blurRadius: 10,
+                          offset: Offset(0,3),
+                        )
+                      ]
                   ),
-                  Container(
-                    width: 190,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text(
-                          "Hot Burger" ,
-                          style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        ),
-                        Text(
-                          "Taste Our Hot Burger,we provide Our Great Foods " ,
-                          style: TextStyle(
-                            fontSize: 16,
-                           // fontWeight: FontWeight.bold,
+                  child: Row(
+                    children: [
+                      InkWell(
+                        onTap: (){},
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: Image.asset(
+                            "images/burger.png",
+                            height: 120,
+                            width: 150,
                           ),
                         ),
-
-                        Text(
-                          "\$10",
-                          style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.red,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        ),
-                    ],
                       ),
-                  ),
+                      Container(
+                        width: 190,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              "Hot Burger" ,
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Taste Our Hot Burger,we provide Our Great Foods " ,
+                              style: TextStyle(
+                                fontSize: 16,
+                                // fontWeight: FontWeight.bold,
+                              ),
+                            ),
 
-
-                  Padding(padding: EdgeInsets.symmetric(vertical: 10 ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Icon(
-                          Icons.favorite_border,
-                          color: Colors.red,
-                          size: 26,
+                            Text(
+                              "\$10",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.red,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
                         ),
-                        Icon(
-                          CupertinoIcons.cart,
-                          color: Colors.red,
-                          size: 26,
+                      ),
+
+
+                      Padding(padding: EdgeInsets.symmetric(vertical: 10 ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(
+                              Icons.favorite_border,
+                              color: Colors.red,
+                              size: 26,
+                            ),
+                            Icon(
+                              CupertinoIcons.cart,
+                              color: Colors.red,
+                              size: 26,
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+
+
+                    ],
+
                   ),
-
-
-                ],
+                ),
 
               ),
-            ),
-
-          ),
 
             Padding(padding: EdgeInsets.symmetric(vertical: 10 ),
               child: Container(
@@ -442,7 +442,7 @@ class   NewestItemWidget extends StatelessWidget{
 
             ),
 
-        ],),
+          ],),
       ),
     );
 
